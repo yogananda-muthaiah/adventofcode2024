e402a243d72957c025126b7376bfb4f67417970f
Muthaiah <yogananda.muthaiah@gmail.com>
2024-12-14 17:32:17 +0100

    day12

---
+++ aoc-day12/index.js
@@ -0,0 +1,69 @@
+sap.ui.define([
+	'sap/ui/core/mvc/Controller',
+	"sap/ui/core/mvc/XMLView",
+	"sap/m/MessageBox",
+	"sap/m/MessageToast"     
+], function(Controller, XMLView, MessageBox) {
+     "use strict";
+
+     Controller.extend("myController", {
+
+		onConfirmationMessageBoxPress: function () {
+			MessageBox.confirm("Approve purchase order 12345?");
+		},
+
+		onAlertMessageBoxPress: function () {
+			MessageBox.alert("The quantity you have reported exceeds the quantity planed.");
+		},
+
+		onErrorMessageBoxPress: function () {
+			MessageBox.error("Select a team in the \"Development\" area.\n\"Marketing\" isn't assigned to this area.");
+		},
+
+		onInfoMessageBoxPress: function () {
+			MessageBox.information("Your booking will be reserved for 24 hours.");
+		},
+
+		onWarningMessageBoxPress: function () {
+			MessageBox.warning("The project schedule was last updated over a year ago.");
+		},
+
+		onSuccessMessageBoxPress: function () {
+			MessageBox.success("Project 1234567 was created and assigned to team \"ABC\".");
+		},
+
+		onResponsivePaddingMessageBox: function () {
+			MessageBox.information("This Message Box has responsive paddings which will adjust based on its content width!", {
+				styleClass: "sapUiResponsivePadding--header sapUiResponsivePadding--content sapUiResponsivePadding--footer"
+			});
+		},
+
+		onError2MessageBoxPress: function () {
+			MessageBox.error("Product A does not exist.", {
+				actions: ["Manage Products", MessageBox.Action.CLOSE],
+				emphasizedAction: "Manage Products",
+				onClose: function (sAction) {
+					MessageToast.show("Action selected: " + sAction);
+				},
+				dependentOn: this.getView()
+			});
+		},
+
+		onWarning2MessageBoxPress: function () {
+			MessageBox.warning("The quantity you have reported exceeds the quantity planned.", {
+				actions: [MessageBox.Action.OK, MessageBox.Action.CANCEL],
+				emphasizedAction: MessageBox.Action.OK,
+				onClose: function (sAction) {
+					MessageToast.show("Action selected: " + sAction);
+				},
+				dependentOn: this.getView()
+			});
+		}
+	});
+
+     XMLView.create({definition: jQuery('#myXml').html()}).then(function (oView) {
+          oView.placeAt("content");
+});
+
+});
+
---
+++ aoc-day12/readme.md
@@ -0,0 +1,17 @@
+
+### Basics of Message Box
+* https://ui5.sap.com/#/api/sap.m.MessageBox
+
+In this example, we are using index.html and index.js with boiler template to render the display of Message Box with Dialog Message Toast.
+
+
+### References & Examples
+* https://ui5.sap.com/#/entity/sap.m.MessageBox/sample/sap.m.sample.MessageBox
+* https://ui5.sap.com/#/entity/sap.m.MessageBox
+
+
+
+
+Disclaimer
+---
+Note: The examples provided are intended to help you learn step-by-step how to use UI5 controls. When designing an actual project, it should be based on the Node.js module.
